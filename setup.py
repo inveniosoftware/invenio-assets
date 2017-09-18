7af0b43978e77a1af68ea033802d1ee098b3897d
Leonardo Rossi <leonardo.r@cern.ch>
2017-09-18 08:56:19 +0200

    installation: pin node-semver to < 0.2.0
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ install_requires = [
     'Flask-Assets>=0.12',
     'Flask-Collect==1.2.2',
     'Flask>=0.11.1',
-    'node-semver>=0.1.1',
+    'node-semver>=0.1.1,<0.2.0',  # FIXME troubleshooting#22
     'speaklater>=1.3',
     'webassets>=0.12',
 ]
