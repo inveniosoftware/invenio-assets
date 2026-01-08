f607dbb75fb39f8e487faea3540a1012eab4fd05
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-08 10:24:39 +0100

    release: v4.2.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,13 +1,17 @@
 ..
     This file is part of Invenio.
     Copyright (C) 2015-2024 CERN.
-    Copyright (C) 2024 Graz University of Technology.
+    Copyright (C) 2024-2026 Graz University of Technology.
 
     Invenio is free software; you can redistribute it and/or modify it
     under the terms of the MIT License; see LICENSE file for more details.
 
 Changes
 =======
+
+Version v4.2.1 (released 2026-01-08)
+
+- setup: pin rspack
 
 Version v4.2.0 (released 2025-04-24)
 
--- invenio_assets/__init__.py
+++ invenio_assets/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2020 CERN.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2026 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -110,7 +110,7 @@ Additionally if we have some static files we should collect them:
 from .ext import InvenioAssets
 from .proxies import current_assets
 
-__version__ = "4.2.0"
+__version__ = "4.2.1"
 
 __all__ = (
     "__version__",
