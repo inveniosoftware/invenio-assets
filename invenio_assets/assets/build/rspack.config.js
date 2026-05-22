46a32a14b7dc1271181ad4b4b55d5f44036cfbf7
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-22 22:59:13 +0200

    chore(licenses): update license headers to use SPDX

--- invenio_assets/assets/build/rspack.config.js
+++ invenio_assets/assets/build/rspack.config.js
@@ -1,11 +1,8 @@
 /*
- * This file is part of Invenio.
- * Copyright (C) 2017-2018 CERN.
- * Copyright (C) 2022-2023 Graz University of Technology.
- * Copyright (C) 2023-2025 TU Wien.
- *
- * Invenio is free software; you can redistribute it and/or modify it
- * under the terms of the MIT License; see LICENSE file for more details.
+ * SPDX-FileCopyrightText: 2017-2018 CERN.
+ * SPDX-FileCopyrightText: 2022-2023 Graz University of Technology.
+ * SPDX-FileCopyrightText: 2023-2025 TU Wien.
+ * SPDX-License-Identifier: MIT
  */
 // https://birtles.blog/2024/08/14/lessons-learned-switching-to-rspack/
 
